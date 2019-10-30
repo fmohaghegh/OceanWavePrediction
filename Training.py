706ffcb0ae70c69e3308af150e64d9095c03eb89
fmohaghegh <40778026+fmohaghegh@users.noreply.github.com>
2019-10-29 17:30:41 -0700

    Removed the unnecessary comments from Training.py

--- Training.py
+++ Training.py
@@ -1,30 +1,3 @@
-"""This code is the revised version of CRNN in the paper:
-  Convolutional RNN: an Enhanced Model for Extracting Features from Sequential Data (https://arxiv.org/abs/1602.05875)
-  Calling the below function is equivalnet to applying one CRNN layer. For a deep model with a few
-  CRNN layers, the function should be invoked multiple times. 
-  Given a tensor, the function extracts patches of `kernel_size` time-steps, and processed each 
-  with one or more recurrent layers. The hidden state of the recurrent neural network is then 
-  returned as the feature vector representing the path. 
-  Args:
-    tensor: The tensor to perform the operation on, shape `[batch, time-steps, features]`
-            or `[batch, time-steps, features, 1]`.
-    kernel_size: The number of time-steps to include in every patch/window (same as in standard 1-D convolution).
-    stride: the number of time-steps between two consecutive patches/windows (same as in standard 1-D convolution).
-    out_channels: The number of extracted features from each patch/window (in standard 1-D convolution 
-                  known as the number of feature maps), which is the hidden dimension of the recurrent 
-                  layers that processes each patch/window.
-    rnn_n_layers: The number of recurrent layers to process the patches/windows. 
-		  (in the original paper was always =1). 
-    rnn_type: Type of recurrent layers to use: `simple`/`lstm`/`gru`
-    bidirectional: Whether to use a bidirectional recurrent layers (such as BLSTM, when the rnn_type is 'lstm'). 
-                   If True, The actual number of extracted features from each patch/window is `2 * out_channels`.
-    w_std: Weights in the recurrent layers will be initialized randomly using a Gaussaian distribution with
-           zero mean and a standard deviation of `w_std`. Biases are initialized with zero. 
-    padding: `SAME` or `VALID` (same as in standard 1-D convolution).
-    scope_name: For variable naming, the name prefix for variables names.  
-  Returns:
-    A 3-D `Tensor` with shape `[batch, time-steps, features]`, similarly to the output of a standard 1-D convolution. 
-  """
 import numpy as np
 import tensorflow as tf
 import csv
@@ -213,31 +186,3 @@ with tf.Session() as sess:
     trseed = trainset[-nstep-ststep:,:,:]
     
     print('Time =    ', time.time()-start)
-
-'''
-
-    if (ststep == 0):
-        X_batch = np.array(trseed[-nstep:,:]).reshape(1, nstep, xsize, 1)
-    else:
-        X_batch = np.array(trseed[-nstep-ststep:-ststep,:,:]).reshape(1, nstep, xsize, 1)
-        
-    y_pred  = sess.run(outputs, feed_dict={X: X_batch})
-    
-    trseed = np.append(trseed,y_pred[0,ststep:nstep,:,:],axis=0)
-
-    for iteration in range(1,nblock):
-        if (ststep == 0):
-            X_batch = np.array(trseed[-nstep:]).reshape(1, nstep, xsize, 1)
-        else:
-            X_batch = np.array(trseed[-nstep-ststep:-ststep,:,:]).reshape(1, nstep, xsize,1)
-                       
-        y_pred  = sess.run(outputs, feed_dict={X: X_batch})
-        trseed  = np.append(trseed,y_pred[0,ststep:nstep,:,:],axis=0)
-        
-results   = trseed[-np.size(trseed,axis=0)+ststep+nstep:,:,:].reshape(np.size(trseed,axis=0)-ststep-nstep,xsize,1)
-
-results   = results[:,:,0]*maxscalar
-
-x = np.linspace(0,3.1415927535*2,xsize)
-
-print('Time =    ', time.time()-start)
